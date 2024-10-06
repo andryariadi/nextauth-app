6d3638762a2770d4a985033d7acd0151edb57419
Andry Ariadi <andryariadi23@gmail.com>
2024-10-07 06:51:44 +0700

    setup nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="b-rose-600">
+      <Button>Hello</Button>
+    </main>
+  );
+}
